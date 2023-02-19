d288c5beca9bbc4dcd19f0f5b2af6fd6244669c1
orenlevi <master.oren@gmail.com>
2023-02-19 08:53:55 +0200

    React training

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -16,6 +16,7 @@ const Expenses = (props) => {
             <ExpensesFilter selected={filteredYear} onFilterChangeHandler={filterChangeHandler}></ExpensesFilter>
             {props.items.map(expense => (
                 <ExpenseItem
+                    key={expense.id}
                     title={expense.title}
                     amount={expense.amount}
                     date={expense.date}
